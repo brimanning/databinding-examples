4cd2fb99adf912cb0f384a06feb8d8e30bce2e8a
Brian Manning <bri@brimanning.com>
2020-02-26 17:09:00 -0500

    Add flip flop

--- app/src/main/java/com/brimanning/mydatabinding/MainViewModel.kt
+++ app/src/main/java/com/brimanning/mydatabinding/MainViewModel.kt
@@ -1,17 +1,37 @@
 package com.brimanning.mydatabinding
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     val count = MutableLiveData<Int>()
     val entryValue = MutableLiveData<String>()
 
+    val flipFlop = MutableLiveData<@StringRes Int>()
+
     init {
         count.value = 0
+        flipOrFlop()
     }
 
     fun incrementCount() {
         count.value = count.value?.inc()
+    }
+
+    private fun flipOrFlop() {
+        if (flipFlop.value == R.string.flip) {
+            flipFlop.value = R.string.flop
+        } else {
+            flipFlop.value = R.string.flip
+        }
+
+        viewModelScope.launch {
+            delay(5000)
+            flipOrFlop()
+        }
     }
 }
